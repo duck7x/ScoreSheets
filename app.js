31efc22618f43a6fff1402c6c0d80e3dcb8daf97
duck7x <hgquack@gmail.com>
2020-05-23 09:26:15 +0000

    Added basic users logic

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express			= require("express"),
 	app				= express(),
+	User			= require("./models/user")
 	seedDB			= require("./seeds"),
+	passport		= require("passport"),
 	mongoose		= require("mongoose"),
 	bodyParser		= require("body-parser"),
+	localStrategy	= require("passport-local"),
 	methodOverride	= require("method-override");
 
 // requiring routes
@@ -16,6 +19,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+
+// passport authentication configuration
+app.use(require("express-session")({
+	secret: "Lun and Timon in the morning!",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+	res.locals.currentUser = req.user;
+	next();
+});
 
 // seedDB();
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,42 @@
-var express	= require("express"),
+var User	= require("../models/user"),
+	express	= require("express"),
 	router	= express();
 
 router.get("/", function(req, res){
 	res.redirect("/games");
+});
+
+router.get("/register", function(req, res){
+	res.render("register");
+});
+
+router.post("/register", function(req, res){
+	var username	= req.body.username,
+		password	= req.body.password
+	User.register(new User({username: username}), password, function(err, user){
+		if(err){
+			console.log(err);
+			res.redirect("/");
+		} else {
+			passport.authenticate("local")(req, res, function(){
+				res.redirect("/");
+			});
+		}
+	});
+});
+
+router.get("/login", function(req, res){
+	res.render("login");
+});
+
+router.post("/login", passport.authenticate("local", {
+	successRedirect: "/",
+	failureRedirect: "/"
+}), function(req, res){});
+
+router.get("/logout", function(req, res){
+	req.logout();
+	res.redirect("/");
 });
 
 module.exports = router;
