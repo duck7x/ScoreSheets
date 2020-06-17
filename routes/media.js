489f82676ede456f50facb240070fc9dcad9afd9
duck7x <hgquack@gmail.com>
2020-06-17 10:31:35 +0000

    Added media management (not delete)

--- routes/media.js
+++ routes/media.js
@@ -5,7 +5,6 @@ var express		= require("express"),
 
 // INDEX - shows all media
 router.get("/", middleware.isAdmin, function(req, res){
-// router.get("/", function(req, res){
 	// res.send("Media page!");
 	exec("ls public/media", function(err, stdout, stderr){
 		if(err){
@@ -19,14 +18,50 @@ router.get("/", middleware.isAdmin, function(req, res){
 });
 
 // NEW - form to add new medias
-router.get("/new", function(req, res){
-// router.get("/new", middleware.isAdmin, function(req, res){
+router.get("/new", middleware.isAdmin, function(req, res){
 	res.render("media/new");
 });
 
 // CREATE - actually adds the new images
 router.post("/", middleware.isAdmin, function(req, res){
-	res.send("Media added!");
+	var count	= req.body.count,
+		skip	= req.body.skip,
+		url		= "",
+		name	= "",
+		type	= "",
+		failed	= 0,
+		success	= 0;
+	
+	// SHOULD MAKE A FUNCTION
+	// SHOULD IMPROVE AND GENERALISE THE UPLOAD FUNCTION
+	for(i = 1; i<= count; i++){
+		if(skip.includes(String(i))){
+			continue;
+		}
+		
+		name = eval("req.body.imageName" + String(i));
+		url = eval("req.body.imageUrl" + String(i));
+		type = url.split(".").slice(-1);
+		exec(`wget ${url} -O public/media/${name}.${type}`, function(err, stdout, stderr){
+			if(err){
+				console.log(err);
+				failed++;
+				req.flash("error", `Failed to upload ${failed} images`);
+			} else {
+				success++;
+				req.flash("success", `${success} images uploaded successfuly`);
+			}
+		});
+	}
+	
+	// if(success > 0){
+	// 	req.flash("success", `${success} images uploaded successfuly`);
+	// }
+	// if(failed > 0){
+	// 	req.flash("error", `Failed to upload ${failed} images`);
+	// }
+	
+	res.redirect("/media");
 });
 
 // DESTROY - deletes media
